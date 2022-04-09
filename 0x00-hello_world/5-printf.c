eae5b3e7ea23dd6752d3a8442f38d9f89a3ff20e
Ejome Great <ejomegare@gmail.com>
2022-04-09 14:48:54 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - uses print to print string
  * Return: return 0
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
