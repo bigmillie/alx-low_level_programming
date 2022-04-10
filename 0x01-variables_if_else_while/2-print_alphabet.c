ce2b55a07911fc053aa3385548ce6d50731b2a0f
Ejome Great <ejomegare@gmail.com>
2022-04-10 11:46:40 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- printing lowercase alphabets
+ * Retuen: returns 0 if program exits well
+ */
+int chat;
+int main(void)
+{
+for (chat = 'a'; chat <= 'z'; chat++)
+{
+putchar(chat);
+}
+return (0);
+}
