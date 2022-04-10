c16683e6c36c45afc07f974638b0d1df282cec76
Ejome Great <ejomegare@gmail.com>
2022-04-10 14:14:59 -0700

    decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print decimal numbers
+ * Return: returns 0 if program exits well
+ */
+int main(void)
+{
+int i;
+for (i=0;i<10;i++)
+{
+printf("%d\n", i);
+}
+return (0);}
