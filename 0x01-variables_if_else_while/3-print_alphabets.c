bb5ff9cd6d0a04eba0db5e5f13f7a95a0e10745f
Ejome Great <ejomegare@gmail.com>
2022-04-10 12:37:36 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- print alphabets
+ * Return: returns 0 if everything goes well
+ */
+int main(void)
+{
+char ch='a';
+char cht = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (cht <= 'Z')
+{
+putchar(cht);
+cht++;
+}
+putchar('\n');
+}
