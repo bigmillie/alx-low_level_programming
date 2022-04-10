e088872892cb31bcbd2ec1e6381cb126a0465c4e
Ejome Great <ejomegare@gmail.com>
2022-04-10 11:55:05 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,13 @@
  * main- printing lowercase alphabets
  * Retuen: returns 0 if program exits well
  */
-int chat;
 int main(void)
 {
+char chat;
 for (chat = 'a'; chat <= 'z'; chat++)
 {
 putchar(chat);
+putchar('\n');
 }
 return (0);
 }
